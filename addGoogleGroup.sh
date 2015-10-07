aa1ef0e1cb0f5c2a64bf6801a15d472b21237456
Mark Y. Goh <mgoh@cca.edu>
2015-10-07 14:11:58 -0700

    allow deny notification

--- addGoogleGroup.sh
+++ addGoogleGroup.sh
@@ -18,22 +18,23 @@ else
 	$GAM update group $GROUP who_can_join invited_can_join
 	$GAM update group $GROUP description "default CCA Google Group"
 	$GAM update group $GROUP who_can_invite all_managers_can_invite
-	$GAM update group $GROUP  allow_external_members false
-	$GAM update group $GROUP  who_can_post_message all_managers_can_post
-	$GAM update group $GROUP  allow_web_posting false
-	$GAM update group $GROUP  primary_language en
-	$GAM update group $GROUP  is_archived true
-	$GAM update group $GROUP  archive_only false
-	$GAM update group $GROUP  reply_to reply_to_sender
-	$GAM update group $GROUP  default_message_deny_notification_text "Your message to the $GROUP group has been rejected."
-	$GAM update group $GROUP  who_can_view_membership all_managers_can_view
-	$GAM update group $GROUP  who_can_leave_group ALL_MANAGERS_CAN_LEAVE
-	$GAM update group $GROUP  include_in_global_address_list false
-	$GAM update group $GROUP  members_can_post_as_the_group false
-	$GAM update group $GROUP  message_moderation_level moderate_none
-	$GAM update group $GROUP  send_message_deny_notification false
-	$GAM update group $GROUP  spam_moderation_level moderate
-	$GAM update group $GROUP  who_can_view_group all_members_can_view
-	$GAM update group $GROUP  show_in_group_directory false
-	$GAM update group $GROUP  max_message_bytes 5M
+	$GAM update group $GROUP allow_external_members false
+	$GAM update group $GROUP who_can_post_message all_managers_can_post
+	$GAM update group $GROUP allow_web_posting false
+	$GAM update group $GROUP primary_language en
+	$GAM update group $GROUP is_archived true
+	$GAM update group $GROUP archive_only false
+	$GAM update group $GROUP reply_to reply_to_sender
+	$GAM update group $GROUP default_message_deny_notification_text "Your message to the $GROUP group has been rejected."
+	$GAM update group $GROUP who_can_view_membership all_managers_can_view
+	$GAM update group $GROUP who_can_leave_group ALL_MANAGERS_CAN_LEAVE
+	$GAM update group $GROUP include_in_global_address_list false
+	$GAM update group $GROUP members_can_post_as_the_group false
+	$GAM update group $GROUP message_moderation_level moderate_none
+	$GAM update group $GROUP send_message_deny_notification false
+	$GAM update group $GROUP spam_moderation_level moderate
+	$GAM update group $GROUP who_can_view_group all_members_can_view
+	$GAM update group $GROUP show_in_group_directory false
+	$GAM update group $GROUP send_message_deny_notification true
+	$GAM update group $GROUP max_message_bytes 5M
 fi
